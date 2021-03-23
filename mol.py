788735f4324e0faa34315e7e407d3c7d71de72ef
Nad Zhou <ahmaddnavid@gmail.com>
2021-03-23 15:46:29 +0500

    Add files via upload

--- mol.py
+++ mol.py
@@ -6,12 +6,23 @@ import time
 
 start = time.time()
 
-suppl = Chem.SDMolSupplier("aspirin.sdf") 
+suppl = Chem.SDMolSupplier("drugs.sdf") 
 x, labels = do_whales.main(suppl, charge_threshold=0, do_charge=True, property_name='')
 
 # x = pd.DataFrame(x, columns=labels)
 
+x = pd.DataFrame(data=x.values, columns=labels)
+x.reset_index(drop=True, inplace=True)
+
 print(x)
+
+x.to_csv("x.csv")
+
+# descriptors = pd.DataFrame()
+# descriptors["descriptors"] = labels
+# descriptors["values"] = x
+
+# descriptors.to_excel("x.xlsx")
 end = time.time()
 
 time_taken = round(end - start, 2)

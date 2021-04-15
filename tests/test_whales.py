48d6f55fc4566417ee0b8d01a39adf1aa16fa21c
nadzhou <ahmaddnavid@gmail.com>
2021-04-15 14:30:24 +0500

    Finally got the tests running. prepare_mol and read_template are super important functions

--- tests/test_whales.py
+++ tests/test_whales.py
@@ -13,10 +13,15 @@ class TestWhales(unittest.TestCase):
     @classmethod
     def setUpClass(cls): 
         cls.whales_inst = Whales("benzene.mol", "benzene.sdf", "./test_results/")
+        cls.whales_inst.read_template()
+        cls.whales_inst.prepare_template()
+        cls.whales_inst.error_in_template()
+        cls.whales_inst.read_library()
+        cls.whales_inst.manipulate()
 
     def test_read_template(self): 
         # Benzene mol to be tested with the class from bezene file
-        mol = "COc1ccc2[nH]cc(CCN(C)C)c2c1"
+        mol = "C1CCCCC1"
         self.whales_inst.read_template()
         self.assertEqual(Chem.MolToSmiles(self.whales_inst.template), mol)
 
@@ -25,11 +30,16 @@ class TestWhales(unittest.TestCase):
         self.assertNotEqual(self.whales_inst.vs_library, self.whales_inst.vs_library_2D)
 
     def test_scaffold_length(self): 
-        expected_results = 20
-        self.whales_inst.prepare_template()
-        self.whales_inst.manipulate()
         
-        # self.assertEqual(len(self.whales_inst.hits), expected_results)
+        self.assertEqual(len(self.whales_inst.norm_whales_library), 7)
+
+    def test_hits_to_scaffold(self): 
+        expected_hits = 7
+        self.whales_inst.calc_euclidean()
+        self.whales_inst.hits_to_scafoold()
+
+        self.assertEqual(len(self.whales_inst.hits), expected_hits)
+
 
 if __name__ == "__main__": 
     unittest.main()
